4c74d40c7e86f3066d4993f2f5dfe9f531fead1a
Neil Pfeiffer <neil.pfeiffer@cerner.com>
2021-07-01 12:47:41 -0500

    Updated to fix IE render and CSS theming issues

--- packages/terra-profile-image/src/ProfileImage.jsx
+++ packages/terra-profile-image/src/ProfileImage.jsx
@@ -47,31 +47,71 @@ const defaultProps = {
   width: '75',
 };
 
-const ProfileImage = (props) => {
-  const theme = React.useContext(ThemeContext);
-
-  // use placeholder SVG as source if default theme, otherwise use background image for all other themes
-  const PlaceholderImageSrc = !theme.className
-    ? 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA3NSA3NSIgd2lkdGg9Ijc1IiBoZWlnaHQ9Ijc1IiBwcmVzZXJ2ZUFzcGVjdFJhdGlvPSJub25lIj48cGF0aCBmaWxsPSIjZGVkZmUwIiBkPSJNMCAwaDc1djc1SDB6Ii8+PGVsbGlwc2UgZmlsbD0iIzliOWZhMSIgY3g9IjM3LjM2IiBjeT0iMjcuNDEiIHJ4PSIxNS45NSIgcnk9IjE2LjM2Ii8+PHBhdGggZmlsbD0iIzliOWZhMSIgIGQ9Ik0zNy41IDUwLjQ1QzIyLjEgNTAuNDUgOS4xMzYgNjAuNjggNS40NSA3NSBoNjQuMSBjLTMuNjgtMTMuOS0xNi42NC0yNC41NSAtMzIuMSAtMjQuNTUgeiIvPjwvc3ZnPg=='
-    : 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA3NSA3NSIgd2lkdGg9Ijc1IiBoZWlnaHQ9Ijc1IiBwcmVzZXJ2ZUFzcGVjdFJhdGlvPSJub25lIj48cGF0aCBmaWxsPSJyZ2JhKDEyNywxMjcsMTI3LDAuMDAwMSkiIGQ9Ik0wIDBoNzV2NzVIMHoiLz48L3N2Zz4=';
-
-  const ProfileImageClassNames = classNames(
-    cx([
-      'profile-image',
-      theme.className,
-    ]),
-    props.className,
-  );
-
-  const ProfileImagePlaceholder = <TerraImage {...props} src={PlaceholderImageSrc} className={[cx(['placeholder-image', props.fit]), ProfileImageClassNames]} />;
-
-  if (props.src) {
-    return (<TerraImage {...props} placeholder={ProfileImagePlaceholder} className={ProfileImageClassNames} />);
+class ProfileImageClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.PlaceholderSpanRef = React.createRef();
+    this.state = { placeholderImageSrc: undefined };
   }
-  return ProfileImagePlaceholder;
-};
 
-ProfileImage.propTypes = propTypes;
-ProfileImage.defaultProps = defaultProps;
+  componentDidMount() {
+    this.getPlaceholderImageSource(this.PlaceholderSpanRef.current);
+  }
 
-export default ProfileImage;
+  getPlaceholderImageSource(currentPlaceholderRef) {
+    if (currentPlaceholderRef) {
+      const placeholderSpanStyles = window.getComputedStyle(currentPlaceholderRef, null);
+      const placeholderImageUrl = placeholderSpanStyles.getPropertyValue('background-image');
+      const placeholderImageString = placeholderImageUrl ? placeholderImageUrl.match(/\((.*?)\)/)[1].replace(/('|")/g, '') : undefined;
+      this.setState({ placeholderImageSrc: placeholderImageString });
+      this.forceUpdate();
+    }
+  }
+
+  createPlaceholderImage(imageClasses) {
+    const {
+      fit, src, ...props
+    } = this.props;
+
+    const theme = this.context;
+
+    if (this.state.placeholderImageSrc) {
+      return (
+        <TerraImage
+          {...props}
+          src={this.state.placeholderImageSrc}
+          fit={fit}
+          className={[cx('placeholder-image'), imageClasses]}
+        />
+      );
+    }
+    return (<span ref={this.PlaceholderSpanRef} className={cx(['placeholder-span', theme.className])} />);
+  }
+
+  render() {
+    const {
+      fit, src, ...props
+    } = this.props;
+
+    const theme = this.context;
+
+    const ProfileImageClassNames = classNames(
+      cx([
+        'profile-image',
+        theme.className,
+      ]),
+      props.className,
+    );
+
+    if (src) {
+      return (<TerraImage {...props} src={src} fit={fit} placeholder={this.createPlaceholderImage(ProfileImageClassNames)} className={ProfileImageClassNames} />);
+    }
+    return (<React.Fragment>{this.createPlaceholderImage(ProfileImageClassNames)}</React.Fragment>);
+  }
+}
+
+ProfileImageClass.propTypes = propTypes;
+ProfileImageClass.defaultProps = defaultProps;
+ProfileImageClass.contextType = ThemeContext;
+
+export default ProfileImageClass;
